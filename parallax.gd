4b90cfd14c5875a37f24b4b69d069945ff2b76a2
RichoM <richi.moran@gmail.com>
2020-12-16 17:06:29 -0300

    Randomized the background noise

--- parallax.gd
+++ parallax.gd
@@ -3,7 +3,12 @@ extends MeshInstance
 export var speed = 1.0
 
 onready var material = get_surface_material(0)
+var initial_pos := Vector3()
+
+func _ready():
+	initial_pos  = global_transform.origin + Vector3(randf() * 100, randf() * 100, 0)
+	visible = randf() > 0.05
 
 func _process(_delta):
-	var position = global_transform.origin
+	var position = initial_pos + global_transform.origin
 	material.set("shader_param/uv1_offset", Vector3(position.x * speed, -position.y * speed, 0))
